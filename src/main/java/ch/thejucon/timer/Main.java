7d6a4e2c389817a117d909ba98c77de8f46c7fda
Thejucon <ju.pa@gmx.ch>
2025-02-24 23:03:57 +0100

    Initial commit

---
+++ src/main/java/ch/thejucon/timer/Main.java
@@ -0,0 +1,20 @@
+package ch.thejucon.timer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+
+    // Plugin load engine
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager();
+        this.getLogger().info("Timer_Plugin loaded!");
+    }
+
+    // Plugin unload engine
+    @Override
+    public void onDisable() {
+        this.getLogger().info("Timer_Plugin saved!");
+    }
+}
